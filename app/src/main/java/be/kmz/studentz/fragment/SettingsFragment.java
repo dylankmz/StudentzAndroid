c09f3facd612cbfe5f85cc5e26d6fd7aa838c874
Dylan Karapekmez <dylankarapekmez@icloud.com>
2021-01-14 01:24:37 +0100

    validation on inputfields added

--- app/src/main/java/be/kmz/studentz/fragment/SettingsFragment.java
+++ app/src/main/java/be/kmz/studentz/fragment/SettingsFragment.java
@@ -1,5 +1,6 @@
 package be.kmz.studentz.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,14 +9,27 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import be.kmz.studentz.R;
 
 public class SettingsFragment extends Fragment {
 
+    private FragmentActivity mContext;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+    public static SettingsFragment newInstance() {
+        return new SettingsFragment();
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mContext = (FragmentActivity) context;
     }
 }
--- app/src/main/java/be/kmz/studentz/fragment/StudentDetailsFragment.java
+++ app/src/main/java/be/kmz/studentz/fragment/StudentDetailsFragment.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -21,6 +22,10 @@ import androidx.navigation.Navigation;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.textfield.TextInputLayout;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import be.kmz.studentz.R;
 import be.kmz.studentz.dialog.ClassroomDialog;
@@ -133,7 +138,8 @@ public class StudentDetailsFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 StudentViewModel model = new ViewModelProvider(mContext).get(StudentViewModel.class);
-                if (selectedStudent == null) {
+                if (selectedStudent == null && checkFirstName() && checkLastName() && checkBirthDate()
+                        && checkEmail() && checkAddress() && checkLocation() && checkZip()) {
                     Student s = new Student(edFirstName.getText().toString(),
                             edLastName.getText().toString(),
                             tvGender.getText().toString(),
@@ -144,8 +150,11 @@ public class StudentDetailsFragment extends Fragment {
                             edAddress.getText().toString(),
                             edLocation.getText().toString(),
                             edZip.getText().toString());
+                    Toast.makeText(mContext, "Student added!", Toast.LENGTH_LONG).show();
                     model.insertStudent(s);
-                } else {
+                    mContext.getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, StudentListFragment.newInstance()).commit();
+                } else if (checkFirstName() && checkLastName() && checkBirthDate()
+                        && checkEmail() && checkAddress() && checkLocation() && checkZip()){
                     selectedStudent.setFirstName(edFirstName.getText().toString());
                     selectedStudent.setLastName(edLastName.getText().toString());
                     selectedStudent.setGender(tvGender.getText().toString());
@@ -157,8 +166,8 @@ public class StudentDetailsFragment extends Fragment {
                     selectedStudent.setLocation(edLocation.getText().toString());
                     selectedStudent.setZip(edZip.getText().toString());
                     model.updateStudent(selectedStudent);
+                    mContext.getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, StudentListFragment.newInstance()).commit();
                 }
-                mContext.getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, StudentListFragment.newInstance()).commit();
             }
         });
 
@@ -196,5 +205,75 @@ public class StudentDetailsFragment extends Fragment {
         }
     };
 
+    //ref voor regex: stackoverflow & https://regexlib.com/Search.aspx?k=street&AspxAutoDetectCookieSupport=1
+    private boolean checkFirstName() {
+        if (!edFirstName.getText().toString().matches("[A-Z][a-z]*")) {
+            firstNameLayout.setError("Please insert a valid first name!");
+            return false;
+        } else {
+            firstNameLayout.setError(null);
+            return true;
+        }
+    }
 
+    private boolean checkLastName() {
+        if (!edLastName.getText().toString().matches("[A-Z][a-z]*")) {
+            lastNameLayout.setError("Please insert a valid last name!");
+            return false;
+        } else {
+            lastNameLayout.setError(null);
+            return true;
+        }
+    }
+
+    private boolean checkBirthDate() {
+        if (!edBirthDate.getText().toString().matches("^[0-3]?[0-9]/[0-3]?[0-9]/(?:[0-9]{2})?[0-9]{2}$")) {
+            birthDateLayout.setError("Please insert a valid birth date!");
+            return false;
+        } else {
+            birthDateLayout.setError(null);
+            return true;
+        }
+    }
+
+    //ref: https://stackoverflow.com/questions/8204680/java-regex-email
+    private boolean checkEmail() {
+        if (!edEmail.getText().toString().matches("^(.+)@(.+)$")) {
+            emailLayout.setError("Please insert a valid email!");
+            return false;
+        } else {
+            emailLayout.setError(null);
+            return true;
+        }
+    }
+
+    private boolean checkAddress() {
+        if (!edAddress.getText().toString().matches("\\A(.*?)\\s+(\\d+[a-zA-Z]?\\s?[-]\\s?\\d*[a-zA-Z]?|\\d+[a-zA-Z-]?\\d*[a-zA-Z]?)")) {
+            addressLayout.setError("Please insert a valid home address and house number!");
+            return false;
+        } else {
+            addressLayout.setError(null);
+            return true;
+        }
+    }
+
+    private boolean checkLocation() {
+        if (!edLocation.getText().toString().matches("[A-Z][a-z]*")) {
+            locationLayout.setError("Please insert a valid location!");
+            return false;
+        } else {
+            locationLayout.setError(null);
+            return true;
+        }
+    }
+
+    private boolean checkZip() {
+        if (!edZip.getText().toString().matches("[0-9]+")) {
+            zipLayout.setError("Please insert a valid belgian zip!");
+            return false;
+        } else {
+            zipLayout.setError(null);
+            return true;
+        }
+    }
 }
